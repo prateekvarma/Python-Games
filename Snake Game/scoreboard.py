00f2ccc808aca76af9dc4cd88c0538774aed99af
prateekvarma <prtk.varma@gmail.com>
2022-10-19 19:24:01 -0600

    Detecting colliding against the wall and display 'game over'

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Ariel", 24, "normal")
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
@@ -15,6 +16,10 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
