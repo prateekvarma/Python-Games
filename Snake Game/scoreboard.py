bcde2e880da0606f3139d17961bcd851ab981b54
prateekvarma <prtk.varma@gmail.com>
2022-10-26 17:21:11 -0600

    Added high score (inc)

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,14 +16,20 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()  # clears the previous score
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0  # reset the score
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()  # clears the previous score
         self.update_scoreboard()
 
