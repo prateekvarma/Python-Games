505f9dda31853285aa0966ff6a767eb1ea4fad5c
prateekvarma <prtk.varma@gmail.com>
2022-10-26 16:55:46 -0600

    Update level, update scoreboard, and write game over on collision with cars

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.move, "Up")
@@ -26,8 +27,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect when turtle crosses & reaches end of y-axis
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
